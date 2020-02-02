46ca754e7f7d898e2dd4aed2b8c572b0ee95447a
Shunchao Wang <wang.2799@osu.edu>
2020-02-01 21:02:43 -0500

    Added ProductDetail page.

--- NorthwindMvc/Controllers/HomeController.cs
+++ NorthwindMvc/Controllers/HomeController.cs
@@ -32,6 +32,20 @@ namespace NorthwindMvc.Controllers
       return View(model);
     }
 
+    public IActionResult ProductDetail(int? id)
+    {
+      if (!id.HasValue)
+      {
+        return NotFound("You must pass a product ID in the route.");
+      }
+      var model = dbContext.Products.SingleOrDefault(p => p.ProductID == id);
+      if (model == null)
+      {
+        return NotFound($"Product with ID of {id} not found.");
+      }
+      return View(model);
+    }
+
     [Route("private")]
     public IActionResult Privacy()
     {
